2f9ace0321d257715d9d0372b3ce97f7804e9af2
Mark Greenway <markgreenway@ligmarine.com>
2017-03-10 18:24:33 -0500

    Working one

---
+++ Brute-numerator/Brute_numerator.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Brute_numerator
+{
+
+    public class BruteSequence : IEnumerable<string>
+    {
+        private Brute_numerator _base;
+        private char[] numbersArray;
+
+        public BruteSequence()
+        {
+            _base = new Brute_numerator();
+        }
+
+        public BruteSequence(char[] currentArray)
+        {
+            _base = new Brute_numerator(currentArray);
+        }
+
+        public ulong NumberGenerated => _base.NumberPasswordsGenerated;
+
+        public IEnumerator<string> GetEnumerator()
+        {
+            return _base;
+        }
+
+
+        IEnumerator<string> IEnumerable<string>.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+
+    public class Brute_numerator : IEnumerator<string>
+    { 
+        private char[] _charactersToTest;
+        private ulong _characterCount;
+
+        private Int16[] _indexes;
+        private string password;
+
+        public ulong NumberPasswordsGenerated = 0;
+
+        public string Current => GetCurrent();
+
+        object IEnumerator.Current => GetCurrent();
+
+        public Brute_numerator()
+        {
+            Setup(CharacterArrays.DefaultArray);
+        }
+
+        public Brute_numerator(char[] defaultArray)
+        {
+            Setup(defaultArray);
+        }
+
+        private void Setup(char[] characterArray, int length = 5)
+        {
+            NumberPasswordsGenerated = 0;
+            _charactersToTest = characterArray;
+            _characterCount = (ulong)characterArray.Length;
+        }
+
+        
+        public bool MoveNext()
+        {
+            Increment();
+            return true;
+        }
+        private string GetCurrent()
+        {
+            return password;
+        }
+        private void Increment()
+        {
+            password = "";
+            ulong current = NumberPasswordsGenerated++;
+
+            do
+            {
+                password = _charactersToTest[current % _characterCount] + password;
+                current /= _characterCount;
+            } while (current != 0);
+        }
+
+        public void Reset()
+        {
+            Setup(_charactersToTest);
+        }
+
+        public void Dispose()
+        {
+           
+        }
+    }
+}
---
+++ Brute-numerator/CharacterArrays.cs
@@ -0,0 +1,47 @@
+﻿namespace Brute_numerator
+{
+    public class CharacterArrays
+    {
+        
+
+        public static char[] LowerCaseLettersArray => new char[]
+        {
+            'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
+        };
+
+        public static char[] UpperCaseLettersArray => new char[]
+        {
+            'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'
+        };
+
+        public static char[] LettersArray => new char[]
+        {
+            'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+            'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'
+        };
+
+        public static char[] NumbersArray => new char[]
+        {
+            '1','2','3','4','5', '6','7','8','9','0'
+        };
+
+        public static char[] AlphanumericArray => new char[]
+        {
+            'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+            'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+            '1','2','3','4','5', '6','7','8','9','0'
+        };
+
+        public static char[] CommonSpecialArray => new char[]
+        {
+            '!','$','#','@','-','%','&'
+        };
+        public static char[] DefaultArray => new char[] 
+        {
+            'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+            'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+            '1','2','3','4','5', '6','7','8','9','0',
+            '!','$','#','@','-' 
+        };
+    }
+}
